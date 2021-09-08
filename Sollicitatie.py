0b1c7a5e831e67f9e7ef464040a7e07cfaff510b
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-08 11:20:59 +0200

    klaar

--- Sollicitatie.py
+++ Sollicitatie.py
@@ -5,37 +5,68 @@ Diploma = input("Bent u in bezit van een MBO-4 diploma ondernemen?")
 rijbewijs = input("Bent u in bezit van een vrachtwagen rijbewijs?")
 hoed = input("Bent u in bezit van een hoge hoed")
 geslacht = input("Bent u een man/vrouw M/V")
-Snor = input("Hoe breed is uw snor?")
+Snor = int(input("Hoe breed is uw snor in cm?"))
 haar = input("Heeft u rood krulhaar?")
-lengte_haar = input("hoelang is uw haar?")
-lengte = input("Hoe lang bent u?")
-gewicht = input("Wat is uw lichaamsgewicht?")
+lengte_haar = int(input("hoelang is uw haar in cm?"))
+lengte = int(input("Hoe lang bent u in cm?"))
+gewicht = int(input("Wat is uw lichaamsgewicht in kg?"))
 cerificaat = input("Heeft u een Certificaat: Overleven met gevaarlijk personeel?")
 
 
 if dieren_dressuur >= 4:
-    1 = True
+    var1 = True
 else:
-    1 = False
+    var1 = False
 if jongleren >= 5:
-    2 = True
+    var2 = True
 else:
-    2 = False
+    var2 = False
 if acrobatiek >= 3:
-    3 = True
+    var3 = True
 else:
-    3 = False
+    var3 = False
 if Diploma == "ja":
-    4 = True
+    var4 = True
 else:
-    4 = False
+    var4 = False
 if rijbewijs == "ja":
-    5 = True
+    var5 = True
 else:
-    5 = False
+    var5 = False
 if hoed == "ja":
-    6 = True
+    var6 = True
 else:
-    6 = False
-if 
+    var6 = False
+if geslacht == "M":
+    if Snor >= 10:
+        var7 = True
+    else:
+        var7 = False
+elif geslacht == "V":
+    if haar == "ja":
+        var8 = True
+        if lengte_haar >= 20:
+            var9 = True
+        else:
+            var9 = False
+    else:
+        var8 = False
+if lengte >= 150:
+    var10 = True
+else:
+    var10 = False
+if gewicht >= 90:
+    var11 = True
+else:
+    var11 = False
+if cerificaat == "ja":
+    var12 = True
+else:
+    var12 = False
+
+
+if var1 or var2 or var3 and var4 and var5 and var6 and var7 or var8 and var9 and var10 and var11 and var12 == True:
+    print("U bent aangenomen.")
+else: 
+    print("U bent niet aangenomen")
 
