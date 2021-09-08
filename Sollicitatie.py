5db985e44b3f7d61e68aceeca30d6b295ecb4e79
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-08 10:32:49 +0200

    commit2

--- Sollicitatie.py
+++ Sollicitatie.py
@@ -1,6 +1,6 @@
-dieren_dressuur = input("Hoeveel jaar heeft u ervaring met dieren_dressuur?")
-jongleren = input("Hoeveel jaar heeft u ervaring met jongleren?")
-acrobatiek = input("Hoeveel jaar heeft u ervaring met acrobatiek?")
+dieren_dressuur = int(input("Hoeveel jaar heeft u ervaring met dieren_dressuur?"))
+jongleren = int(input("Hoeveel jaar heeft u ervaring met jongleren?"))
+acrobatiek = int(input("Hoeveel jaar heeft u ervaring met acrobatiek?"))
 Diploma = input("Bent u in bezit van een MBO-4 diploma ondernemen?")
 rijbewijs = input("Bent u in bezit van een vrachtwagen rijbewijs?")
 hoed = input("Bent u in bezit van een hoge hoed")
@@ -14,4 +14,28 @@ cerificaat = input("Heeft u een Certificaat: Overleven met gevaarlijk personeel?
 
 
 if dieren_dressuur >= 4:
-    
+    1 = True
+else:
+    1 = False
+if jongleren >= 5:
+    2 = True
+else:
+    2 = False
+if acrobatiek >= 3:
+    3 = True
+else:
+    3 = False
+if Diploma == "ja":
+    4 = True
+else:
+    4 = False
+if rijbewijs == "ja":
+    5 = True
+else:
+    5 = False
+if hoed == "ja":
+    6 = True
+else:
+    6 = False
+if 
+
