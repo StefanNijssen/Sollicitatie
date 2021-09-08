3ac403c32f5c2d371c5d0b9c625e2fad73dcd929
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-09-08 10:21:22 +0200

    commit1

---
+++ Sollicitatie.py
@@ -0,0 +1,17 @@
+dieren_dressuur = input("Hoeveel jaar heeft u ervaring met dieren_dressuur?")
+jongleren = input("Hoeveel jaar heeft u ervaring met jongleren?")
+acrobatiek = input("Hoeveel jaar heeft u ervaring met acrobatiek?")
+Diploma = input("Bent u in bezit van een MBO-4 diploma ondernemen?")
+rijbewijs = input("Bent u in bezit van een vrachtwagen rijbewijs?")
+hoed = input("Bent u in bezit van een hoge hoed")
+geslacht = input("Bent u een man/vrouw M/V")
+Snor = input("Hoe breed is uw snor?")
+haar = input("Heeft u rood krulhaar?")
+lengte_haar = input("hoelang is uw haar?")
+lengte = input("Hoe lang bent u?")
+gewicht = input("Wat is uw lichaamsgewicht?")
+cerificaat = input("Heeft u een Certificaat: Overleven met gevaarlijk personeel?")
+
+
+if dieren_dressuur >= 4:
+    
